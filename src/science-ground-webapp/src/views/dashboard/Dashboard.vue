c473ddabbe3472d8a67e2dbdb8278994866bf55e
Luca <oooiii891@gmail.com>
2021-05-22 09:53:24 +0200

    Initial Commit webapp

---
+++ src/science-ground-webapp/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container
+    id="dashboard"
+    fluid
+    tag="section"
+  >
+    <v-row>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardDashboard',
+
+    data () {
+      return {}
+    },
+
+    methods: {
+      complete (index) {
+        this.list[index] = !this.list[index]
+      },
+    },
+  }
+</script>
---
+++ src/science-ground-webapp/src/views/dashboard/components/core/AppBar.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-app-bar
+    id="app-bar"
+    absolute
+    app
+    color="transparent"
+    flat
+    height="75"
+  >
+    <v-btn
+      class="mr-3"
+      elevation="1"
+      fab
+      small
+      @click="setDrawer(!drawer)"
+    >
+      <v-icon v-if="value">
+        mdi-view-quilt
+      </v-icon>
+
+      <v-icon v-else>
+        mdi-dots-vertical
+      </v-icon>
+    </v-btn>
+
+    <v-toolbar-title
+      class="hidden-sm-and-down font-weight-light"
+      v-text="$route.name"
+    />
+
+    <v-spacer />
+    <div class="mx-3" />
+
+    <v-menu
+      bottom
+      left
+      offset-y
+      origin="top right"
+      transition="scale-transition"
+    >
+      <v-list
+        :tile="false"
+        nav
+      >
+        <div>
+          <app-bar-item
+            v-for="(n, i) in notifications"
+            :key="`item-${i}`"
+          >
+            <v-list-item-title v-text="n" />
+          </app-bar-item>
+        </div>
+      </v-list>
+    </v-menu>
+
+  </v-app-bar>
+</template>
+
+<script>
+  // Components
+  import { VHover, VListItem } from 'vuetify/lib'
+
+  // Utilities
+  import { mapState, mapMutations } from 'vuex'
+
+  export default {
+    name: 'DashboardCoreAppBar',
+
+    components: {
+      AppBarItem: {
+        render (h) {
+          return h(VHover, {
+            scopedSlots: {
+              default: ({ hover }) => {
+                return h(VListItem, {
+                  attrs: this.$attrs,
+                  class: {
+                    'black--text': !hover,
+                    'white--text secondary elevation-12': hover,
+                  },
+                  props: {
+                    activeClass: '',
+                    dark: hover,
+                    link: true,
+                    ...this.$attrs,
+                  },
+                }, this.$slots.default)
+              },
+            },
+          })
+        },
+      },
+    },
+
+    props: {
+      value: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
+    data: () => ({
+      notifications: [
+        'Mike John Responded to your email',
+        'You have 5 new tasks',
+        'You\'re now friends with Andrew',
+        'Another Notification',
+        'Another one',
+      ],
+    }),
+
+    computed: {
+      ...mapState(['drawer']),
+    },
+
+    methods: {
+      ...mapMutations({
+        setDrawer: 'SET_DRAWER',
+      }),
+    },
+  }
+</script>
