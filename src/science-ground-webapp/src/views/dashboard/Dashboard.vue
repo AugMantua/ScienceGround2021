3d0fc5f0f5499e1251c5e55e4c9d5d3c93dadaaa
Luca <oooiii891@gmail.com>
2021-05-22 14:30:20 +0200

    aggiunto librerie grafici
    eliminazione librerie inutilizzate aggiunto libreria grafici

--- src/science-ground-webapp/src/views/dashboard/Dashboard.vue
+++ src/science-ground-webapp/src/views/dashboard/Dashboard.vue
@@ -5,22 +5,32 @@
     tag="section"
   >
     <v-row>
+      <apexchart width="500" type="bar" :options="chartOptions" :series="series"></apexchart>
     </v-row>
   </v-container>
 </template>
 
 <script>
+  import VueApexCharts from 'vue-apexcharts';
+
   export default {
     name: 'DashboardDashboard',
-
+    components: { 'apexchart': VueApexCharts},
     data () {
-      return {}
-    },
-
-    methods: {
-      complete (index) {
-        this.list[index] = !this.list[index]
-      },
+     return{
+       chartOptions: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91]
+        }]
+     }
     },
   }
 </script>
