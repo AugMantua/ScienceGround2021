d62ce84634ef5ff83fa3863a30f742e1d44c4eb8
Pietro Marchini <pietro.marchini94@gmail.com>
2021-06-18 10:58:54 +0200

    CORS -> AllowAll -> tested ok

--- src/server/src/server.go
+++ src/server/src/server.go
@@ -10,11 +10,6 @@ import (
 
 func main() {
 
-	/*CORS*/
-	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-	})
-
 	//DB related
 	SqliteDB := dataDBinit(_DB_NAME)
 	//Closes db at the end of main
@@ -24,7 +19,7 @@ func main() {
 	mux.HandleFunc("/data/measures/add", AddMeasure(SqliteDB))
 	mux.HandleFunc("/data/measures/get", RequestMeasures(SqliteDB))
 	mux.HandleFunc("/data/terrariums/get", RequestTerrariumsList(SqliteDB))
-	fmt.Println("Serving on port 80")
-	err := http.ListenAndServe(":80", c.Handler(mux))
+	fmt.Println("Serving on port 8080")
+	err := http.ListenAndServe(":8080", cors.AllowAll().Handler(mux))
 	log.Fatal(err)
 }
