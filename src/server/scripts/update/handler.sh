552016bebb85becaa6156b66e8de9d77a02fe19d
Pietro Marchini <p.marchini@tecmasolutions.com>
2022-08-17 16:12:05 +0200

    perf: handler

--- src/server/scripts/update/handler.sh
+++ src/server/scripts/update/handler.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-COMPOSE_FILE_PATH=${HOME}/rancher/ScienceGround2021/src/server/docker-compose.yml
+COMPOSE_FILE_PATH=${HOME}/ScienceGround2021/src/server/docker-compose.yml
 sudo rm -r -f ${HOME}/ScienceGround2021 
 docker run -ti --rm -v ${HOME}:/rancher -v $(pwd):/git alpine/git clone https://github.com/AugMantua/ScienceGround2021.git
 docker  run  -v ${COMPOSE_FILE_PATH}:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 build
