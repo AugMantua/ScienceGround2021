d9415526af8b0acea913ab693bba6e4fd9f76dbc
Pietro Marchini <p.marchini@tecmasolutions.com>
2022-08-17 16:52:13 +0200

    perf: minor

--- src/server/scripts/update/handler.sh
+++ src/server/scripts/update/handler.sh
@@ -7,4 +7,5 @@ docker run -ti --rm -v ${HOME}:/rancher -v $(pwd):/git alpine/git clone https://
 cd ${COMPOSE_FILE_PATH}
 docker  run --rm  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 build
 docker  run --rm  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 down
-docker  run --rm -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 up
+docker  run --rm -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 up --no-start
+docker  run --rm -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 start
