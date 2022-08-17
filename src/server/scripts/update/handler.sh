69a503e0eae0ba410018e3737c8e6b664a039994
Pietro Marchini <p.marchini@tecmasolutions.com>
2022-08-17 15:57:01 +0200

    perf: updater

--- src/server/scripts/update/handler.sh
+++ src/server/scripts/update/handler.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-COMPOSE_FILE_PATH=${HOME}/ScienceGround2021/src/server/ 
+COMPOSE_FILE_PATH=${HOME}/ScienceGround2021/src/server/docker-compose.yml
 sudo rm -r -f ${HOME}/ScienceGround2021 
 docker run -ti --rm -v ${HOME}:/rancher -v $(pwd):/git alpine/git clone https://github.com/AugMantua/ScienceGround2021.git
-docker  run  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 ${COMPOSE_FILE_PATH} build
-docker  run  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 ${COMPOSE_FILE_PATH} down
-docker  run  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 ${COMPOSE_FILE_PATH} up
+docker  run  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 build ${COMPOSE_FILE_PATH}
+docker  run  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 down ${COMPOSE_FILE_PATH}
+docker  run  -v `pwd`:`pwd` -v /var/run/docker.sock:/var/run/docker.sock -w `pwd` docker/compose:alpine-1.27.1 up ${COMPOSE_FILE_PATH}
